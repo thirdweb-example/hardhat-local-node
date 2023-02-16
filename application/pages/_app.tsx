e45d5eeb29db7b3053e3d55ba917b93d64157651
Jarrod Watts <jarrodwatts16@gmail.com>
2023-02-16 13:59:36 +1100

    fix thirdweb provider

--- application/pages/_app.tsx
+++ application/pages/_app.tsx
@@ -1,11 +1,10 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { Localhost } from "@thirdweb-dev/chains";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={Localhost}>
+    <ThirdwebProvider activeChain="localhost">
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
